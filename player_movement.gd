79f80d4afb13e490789b63745fffcdaa5d814743
Lukáš Jandourek <Crazyk852@email.cz>
2023-04-29 18:01:36 +0200

    Box and new player controller

--- player_movement.gd
+++ player_movement.gd
@@ -1,12 +1,13 @@
-extends CharacterBody2D
+extends RigidBody2D
 
-var gravity = 250;
-var force = 2;
-var speed = 150;
-var jump_amount = 300;
+var speed = 20;
+var jump_amount = 650;
 
 var grounded = false;
 var area_collision_count = 0;
+var direction = 1;
+
+var air_resistence = 0.01;
 
 func _ready():
 	pass
@@ -15,37 +16,31 @@ func _ready():
 func _process(delta):
 	
 	# If we're standing over at least one RigidBody/StaticBody, we're grounded
-	if (area_collision_count > 0):
+	if (area_collision_count > 1):
 		grounded = true;
 	else:
 		grounded = false;
 	
-	# Gravity
-	velocity.y += gravity * delta;
-	
 	# Inputs
 	if (Input.is_key_pressed(KEY_LEFT)):
-		velocity.x = -speed;
+		apply_central_impulse(Vector2.LEFT * speed)
+		direction = -1;
 	if (Input.is_key_pressed(KEY_RIGHT)):
-		velocity.x = speed;
-	if (not Input.is_key_pressed(KEY_LEFT) and not Input.is_key_pressed(KEY_RIGHT)):
-		# Only stop the player if grounded
-		if grounded:
-			velocity.x = 0;
-			
-	# Movement and collisions
-	if move_and_slide():
-		for i in get_slide_collision_count():
-			var col = get_slide_collision(i)
-			if col.get_collider() is RigidBody2D:
-				# Apply forces to RigidBody
-				col.get_collider().apply_force(col.get_normal() * -velocity.length() * force)
+		apply_central_impulse(Vector2.RIGHT * speed)
+		direction = 1;
+		
+	get_node("Torso").scale.x = direction * abs(get_node("Torso").scale.x)
+	get_node("Legs").scale.x = direction * abs(get_node("Legs").scale.x)
+	
+	apply_central_impulse(linear_velocity * -1 * air_resistence)
+		
+	
 
 func _input(event):
 	# Jumping
 	var just_pressed = event.is_pressed() and not event.is_echo()
-	if Input.is_key_pressed(KEY_UP) and just_pressed and grounded:
-		velocity.y = -jump_amount;
+	if Input.is_key_pressed(KEY_UP) and just_pressed and grounded and linear_velocity.y > -100:
+		apply_central_impulse(Vector2.UP * jump_amount)
 
 
 # Check how many RigidBody or StaticBody objects the ray is intersecting (to check if grounded)
