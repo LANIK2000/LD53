f3e1c7ec9e3c8dda81d54a89878abc61295e6fb0
Lukáš Jandourek <Crazyk852@email.cz>
2023-05-01 16:56:57 +0200

    Reverse button (experimental)

--- Scenes/truck.gd
+++ Scenes/truck.gd
@@ -8,12 +8,17 @@ var things = 0;
 var acceleration = 1300;
 var gass_timer = 0;
 
+var direction = 1;
+
 func _ready():
 	audio_player.pitch_scale = .5
 	audio_player.volume_db = -10
 
 func _process(delta):
-	pass
+	if direction == 1:
+		$TruckBody/DirectionButton/Button.modulate = Color(0,1,0);
+	else:
+		$TruckBody/DirectionButton/Button.modulate = Color(1,0,0);
 
 func _physics_process(delta):
 	
@@ -31,7 +36,15 @@ func _physics_process(delta):
 	
 	if (things > 0):
 		gass_timer = 1
-		$Wheel1.apply_torque_impulse(acceleration * delta * 100);
-		$Wheel2.apply_torque_impulse(acceleration * delta * 100);
-		$Wheel3.apply_torque_impulse(acceleration * delta * 100);
+		$Wheel1.apply_torque_impulse(direction * acceleration * delta * 100);
+		$Wheel2.apply_torque_impulse(direction * acceleration * delta * 100);
+		$Wheel3.apply_torque_impulse(direction * acceleration * delta * 100);
 	
+
+# Change direction
+func _on_direction_button_body_exited(body):
+	direction = -direction;
+
+
+func _on_direction_button_body_entered(body):
+	direction = -direction;
